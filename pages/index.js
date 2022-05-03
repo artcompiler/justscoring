fc3f9bedb04c66376bb811f62ad9f937251df3a8
Jeff Dyer <jeffdyer@acm.org>
2022-05-03 11:16:12 -0700

    Add PurgeCSS

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="text-3xl font-bold font-mono text-orange-700 underline">
+      hello, world!
     </h1>
   )
 }
