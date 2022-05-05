954630837d453e3d128dae920414c4b5e3289134
Jeff Dyer <jeffdyer@acm.org>
2022-05-05 14:23:00 -0700

    Cleanup

---
+++ pages/goodbye.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold font-mono text-orange-600 underline">
+      goodbye, world!!!
+    </h1>
+  );
+}
--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold font-mono text-orange-700 underline">
-      hello, world!
-    </h1>
-  );
-}
