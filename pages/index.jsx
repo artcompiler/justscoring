8a4671736a575989731bc586fcb1f7457140456a
Jeff Dyer <jeffdyer@acm.org>
2022-05-12 11:04:21 -0700

    Rename

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className="justify-center pt-28">
       <h1 className="text-center align-middle text-3xl font-bold font-mono text-gray-500">
-        ONLYSCORING.COM
+        JUSTSCORING.COM
       </h1>
        <p className="text-center align-middle font-mono text-gray-400">
-        SCORE ANYTHING.
+        SCORE ANYTHING. MACHINE LEARNING ASSISTANT.
         COMING SOON
       </p>
     </div>
