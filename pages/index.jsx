0faff4a40fb8697e25eee395b41f4bd904672f9d
Jeff Dyer <jeffdyer@acm.org>
2022-05-12 14:47:30 -0700

    Fix &apos;

--- pages/index.jsx
+++ pages/index.jsx
@@ -143,7 +143,7 @@ export default function Home() {
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span className="block xl:inline">Quizzes graded</span>{' '}
-              <span className="block text-indigo-600 xl:inline">so you dont have to.</span>
+              <span className="block text-indigo-600 xl:inline">so you don&apos;t have to.</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Automatically grade quizzes delivered to your students on popular platforms like Google, Learnosity, Moodle, and more.
