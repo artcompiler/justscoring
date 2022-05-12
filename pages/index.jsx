9e0f23fc2e20eb90c4d84f2896a3ce64731ec3f5
Jeff Dyer <jeffdyer@acm.org>
2022-05-12 14:25:58 -0700

    Fix build

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon, AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline'
+import { Image } from 'next/image'
 
 const navigation = [
   { name: 'About', href: '#' },
@@ -9,7 +10,6 @@ const navigation = [
 
 export default function Home() {
   return (
-    <>
     <div className="relative bg-gray-50 overflow-hidden">
       <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
         <div className="relative h-full max-w-7xl mx-auto">
@@ -66,11 +66,7 @@ export default function Home() {
                 <div className="flex items-center justify-between w-full md:w-auto">
                   <a href="#">
                     <span className="sr-only">Workflow</span>
-                    <img
-                      className="h-8 w-auto sm:h-10"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                      alt=""
-                    />
+                    <XIcon className="h-6 w-6" />
                   </a>
                   <div className="-mr-2 flex items-center md:hidden">
                     <Popover.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -115,17 +111,10 @@ export default function Home() {
             >
               <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                 <div className="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                      alt=""
-                    />
-                  </div>
                   <div className="-mr-2">
                     <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                       <span className="sr-only">Close menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                      <XIcon className="h-6 w-6" />
                     </Popover.Button>
                   </div>
                 </div>
@@ -155,7 +144,7 @@ export default function Home() {
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span className="block xl:inline">Quizzes graded</span>{' '}
-              <span className="block text-indigo-600 xl:inline">so you don't have to.</span>
+              <span className="block text-indigo-600 xl:inline">so you dont have to.</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Automatically grade quizzes delivered to your students on popular platforms like Google, Learnosity, Moodle, and more.
@@ -182,8 +171,6 @@ export default function Home() {
       </main>
       </div>
     </div>
-      <Features/>
-      </>
   )
 }
 
@@ -216,7 +203,7 @@ const features = [
 
 function Features() {
   return (
-    <div className="hidden py-12 bg-white">
+    <div className="py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
