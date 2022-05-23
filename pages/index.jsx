823a85baba96b18d4ba0a6e6be73f3ea06224b5c
Jeff Dyer <jeffdyer@acm.org>
2022-05-23 13:57:12 -0700

    Tweak text

--- pages/index.jsx
+++ pages/index.jsx
@@ -151,7 +151,7 @@ export default function Home() {
       <span className="block text-indigo-600 xl:inline">so you don&apos;t have to.</span>
       </h1>
       <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-3xl md:max-w-xl">
-      Automatic scoring of short answer responses in Google Forms.
+      Automatic scoring of short answer responses to Google Forms.
       </p>
       <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
       <div className="rounded-md shadow">
