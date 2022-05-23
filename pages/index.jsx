cfe44c4d5c7563402909e56f77c8548103a2226a
Jeff Dyer <jeffdyer@acm.org>
2022-05-23 12:23:51 -0700

    Tweak text

--- pages/index.jsx
+++ pages/index.jsx
@@ -147,7 +147,7 @@ export default function Home() {
       <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
       <div className="text-center">
       <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-      <span className="block xl:inline">Short answers graded</span>{' '}
+      <span className="block xl:inline">Automatic short answers graded</span><br />
       <span className="block text-indigo-600 xl:inline">so you don&apos;t have to.</span>
       </h1>
       <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
